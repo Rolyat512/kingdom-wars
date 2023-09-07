b031db33b8efeff93dd8ad60edbd715ad651af08
Rolyat512 <122414452+Rolyat512@users.noreply.github.com>
2023-09-06 22:27:37 -0500

    base folder structure

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import styles from "./styles";
+
+const Header = () => {
+  return (
+    <div>
+      Hello 
+    </div>
+  )
+}
+
+export default Header
