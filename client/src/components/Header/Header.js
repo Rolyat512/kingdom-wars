eb9c2df743ca38daa121cc053d4afbfb3d15ba28
Rolyat512 <122414452+Rolyat512@users.noreply.github.com>
2023-09-22 20:09:25 -0500

    added color transition to title

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { Box } from "@mui/material";
 import styles from "./styles";
+import "./header.css"
 
 const Header = () => {
   return (
     <Box sx={{ ...styles.mainContainer }}>
-      <div> Kingdom Wars </div>
+      <div className="headerTitle"> Kingdom Wars </div>
     </Box>
   )
 }
