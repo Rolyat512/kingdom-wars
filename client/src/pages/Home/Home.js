229aca452bb5ada22929545787af93a53d4af1bf
Rolyat512 <122414452+Rolyat512@users.noreply.github.com>
2023-09-22 20:22:44 -0500

    added a few buttons which will eventually be the skills

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,18 +1,28 @@
 import React from "react";
-import { Box } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 import styles from "./styles";
-import Timer from '../../components/Timer/Timer';
-
+import Timer from "../../components/Timer/Timer";
 
 const Home = () => {
 	return (
 		<Box sx={{ ...styles.mainContainer }}>
-			<Box sx={{ ...styles.skillBox }}>
-                <Timer />
-            </Box>
+			<Button variant="contained" sx={{ ...styles.button }}>Wood Working</Button>
+			<Button variant="contained" sx={{ ...styles.button }}> Cartography </Button>
+			<Button variant="contained" sx={{ ...styles.button }}> Sailing</Button>
+			<Button variant="contained" sx={{ ...styles.button }}>Fishing</Button>
+			<Button variant="contained" sx={{ ...styles.button }}>Astronomy</Button>
 		</Box>
 	);
 };
 
 export default Home;
 
+// const Home = () => {
+// 	return (
+// 		<Box sx={{ ...styles.mainContainer }}>
+// 			<Box sx={{ ...styles.skillBox }}>
+//                 <Timer />
+//             </Box>
+// 		</Box>
+// 	);
+// };
--- client/src/pages/Home/styles.js
+++ client/src/pages/Home/styles.js
@@ -2,13 +2,23 @@ const styles = {
     mainContainer: {
         width: "100%", 
         height: '90vh',
-        background: '#ffebee'
+        background: '#ffebee',
+        // display: 'column',
+        justifyContent: 'center',
+        
+
     },
     skillBox: {
         border: '1px solid',
         width: '300px',
         height: '300px',
         color: 'black'
+    },
+    button: {
+        backgroundColor: 'red',
+        height: '70px',
+        width: '500px',
+        margin: '10px'
     }
 }
 
